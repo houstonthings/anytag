79ed095de875fc23e6f3d54eae14e8b114b5c254
Jamie Houston <foresterh@gmail.com>
2014-05-19 19:03:41 -0700

    got view working with api updates

--- static/js/app.js
+++ static/js/app.js
@@ -1,10 +1,21 @@
 app = angular.module('anytag.app.static', []);
 app.controller('AppController', ['$scope', '$http', function($scope, $http){
-    $http.get('/api/items').then(function(result){
-        $scope.items = [];
-        _.each(result.data, function(item){
-            $scope.items.push(item);
+    $http.get('/api/tags/').then(function(result){
+        $scope.tags = [];
+        _.each(result.data, function(val){
+            $scope.tags.push(val);
         });
-        $scope.listparent = $scope.items[0];
+    });
+    $http.get('/api/items/').then(function(result){
+        $scope.items = [];
+        _.each(result.data, function(val){
+            $scope.items.push(val);
+        });
+    });
+    $http.get('/api/tagvalues/').then(function(result){
+        $scope.tagvalues = [];
+        _.each(result.data, function(val){
+            $scope.tagvalues.push(val);
+        });
     });
 }]);
